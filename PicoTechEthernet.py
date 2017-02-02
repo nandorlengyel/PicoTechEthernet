cc42663e262dec54b4fcb633394675b2bcf1d5ea
David Lutton <david@dalun.space>
2017-02-02 19:49:22 +0000

    Refactor via testing with device

--- PicoTechEthernet.py
+++ PicoTechEthernet.py
@@ -1,17 +1,32 @@
 import socket
+import binascii
 
 
 class PicoTechEthernet():
 
-    def __init__(self, ip='192.168.0.200', port=6554):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # prepare socket for UDP
-        self.socket.connect((ip, port))  # Connect
+    def __init__(self, model=b"CM3", ip='192.168.0.200', port=6554):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # prepare socket for UDP
+        self.model = model
+        self.ip = ip
+        self.port = port
+
+    def connect(self):
+        self.socket.connect((self.ip, self.port))  # Connect
+        self.socket.send("\x00".encode('ascii'))  # Greet
+        recv = self.socket.recv(200)
+        if recv.startswith(self.model):
+            return(True)
+        else:
+            print(recv)
+            return(False)
 
     def lock(self):
         self.socket.send("lock\x00".encode('ascii'))  # Acquire lock
-        if self.socket.recv(200) == b"Lock Success\x00":
+        recv = self.socket.recv(200)
+        if recv == b"Lock Success\x00" or recv == b'Lock Success (already locked to this machine)\x00':
             return(True)
         else:
+            print(recv)
             return(False)
 
     def alive(self):
@@ -23,11 +38,14 @@ class PicoTechEthernet():
             print(recv)  # what did it respond with instead
             return(False)
 
-    def channelsetup(self, CH=1, mode=1):
-        pass
-        # self.socket.send("\x31".encode('ascii'))
-        # b"Converting\x00"
-        # b"Mains Changed\x00"
+    def channelsetup(self):
+        self.socket.send(bytes("1w", 'ascii'))
+        recv = self.socket.recv(60)
+        if recv == b'Converting\x00':
+            return(True)
+        else:
+            print(recv)
+            return(False)
 
     def filter(self, Hz=50):
         if filter == 50:
@@ -35,12 +53,21 @@ class PicoTechEthernet():
         else:
             if filter == 60:
                 self.socket.send("\x30\x01".encode('ascii'))
+        # print(self.socket.recv(136))
 
     def EEPROM(self):
         self.socket.send("2".encode('ascii'))  # Ask for EEPROM of device
-        self.EEPROM = socket.recv(136)
+        self.EEPROM = self.socket.recv(136)
         # Parse EEPROM
         return(self.EEPROM)
+
+    def read(self):
+        recv = self.socket.recv(60)
+        if len(recv) == 20:  # assume len of 20 is valid data
+            return(binascii.hexlify(recv).decode())  # 0820090a590920090b120a20090c4f0b2009090d
+        else:
+            print(recv)
+            return(False)
 
 # \x33
 '''
@@ -56,11 +83,11 @@ class PicoTechEthernet():
 
 
 class PicoTechEthernetFind():
-    def __init__(self, ip='255.255.255.255', port=23):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # prepare socket for UDP
-        self.socket.connect((ip, port))  # Connect
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # prepare socket for UDP
 
-    def find(self):
+    def find(self, ip='255.255.255.255', port=23):
+        self.socket.connect((ip, port))  # Connect
         self.socket.send("0x666666".encode('ascii'))
         responce = self.socket.recv(200)
         # responce = b"fffffffffffffffffffffffffffffffffffffffffffffffffffffffffa199affffffffffffffffffffffffffffffffffff"
@@ -69,4 +96,4 @@ class PicoTechEthernetFind():
         print(port)
 
 # find = PicoTechEthernetFind()
-# find.find()
+# find.find(ip='192.168.57.200')
