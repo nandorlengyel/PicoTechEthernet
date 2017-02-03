cca5de61ebb2529889675f9b5ad8302510f24a7d
David Lutton <david@dalun.space>
2017-02-03 22:41:04 +0000

    Move decode, add generator function

--- PicoTechEthernet.py
+++ PicoTechEthernet.py
@@ -10,6 +10,14 @@ class PicoTechEthernet():
         self.ip = ip
         self.port = port
 
+    def decode(self, data="00200059120120005b8b0220005ce30320005b97"):  # = CH0: ~ 0.218 mV
+        channel, zero, one, two, three = data[0:2], int(data[3:10], 16), int(data[13:20], 16), int(data[23:30], 16), int(data[33:40], 16)
+
+        result = (2.5 * (float(zero+one+two+three) / 4) * 1000) / 2**28
+
+        channeloffset = {"00": 0, "04": 1, "08": 2}
+        return(channeloffset[channel], result)
+
     def connect(self):
         self.socket.connect((self.ip, self.port))  # Connect
         self.socket.send("\x00".encode('ascii'))  # Greet
@@ -68,10 +76,21 @@ class PicoTechEthernet():
     def read(self):
         recv = self.socket.recv(60)
         if len(recv) == 20:  # assume len of 20 is valid data
-            return(binascii.hexlify(recv).decode())  # 0820090a590920090b120a20090c4f0b2009090d
+            return(binascii.hexlify(recv).decode())
         else:
             print(recv)
             return(False)
+
+    def generator(self):
+        while [True]:  # Loop forever
+
+            print(self.alive())
+            for _ in range(12):  # every 12 reads send/read a keepalive
+                read = self.read()
+                if read is not False:
+                    channel, value = self.decode(read)
+                    yield([channel, value])
+
 
 # \x33
 '''
@@ -84,20 +103,3 @@ class PicoTechEthernet():
 \x3133
 \x3177
 '''
-
-
-class PicoTechEthernetFind():
-    def __init__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # prepare socket for UDP
-
-    def find(self, ip='255.255.255.255', port=23):
-        self.socket.connect((ip, port))  # Connect
-        self.socket.send("0x666666".encode('ascii'))
-        response = self.socket.recv(200)
-        # response = b"fffffffffffffffffffffffffffffffffffffffffffffffffffffffffa199affffffffffffffffffffffffffffffffffff"
-        port = int(response[58:62], 16)
-        # recover the responding ip address, somehow?
-        print(port)
-
-# find = PicoTechEthernetFind()
-# find.find(ip='192.168.57.200')
--- client.py
+++ client.py
@@ -2,42 +2,16 @@ from PicoTechEthernet import PicoTechEthernet as PicoTechEthernet
 
 CM3 = PicoTechEthernet(model=b"CM3", ip='192.168.57.200', port=6554)
 
-print("Connect")
 print(CM3.connect())
-
-print("Lock")
 print(CM3.lock())
-
-print("Filter")
 CM3.filter(50)
-
-print("EEPROM")
-print(CM3.EEPROM())
-
-print("Set")
+# print(CM3.EEPROM())
 CM3.set("1w", b'Converting\x00')  # needs channel setup information
 
 
-while [True]:  # Loop forever
-
-    print(CM3.alive())
-    for _ in range(12):  # every 12 reads send/read a keepalive
-        read = CM3.read()
-        if read is not False:
-            print(read)  # receive readback as hexadecimal
-            # channel, result = CM3.decode(read)
-
-'''
-042004dc88052004d940062004d789072004d767
-082008dcfb092008ddbc0a2008e0ba0b2008df0c
-002003541201200356d502200350090320034fc4
-042004d63f052004d7b0062004da0e072004d986
-082008e2c9092008e2940a2008de850b2008dd5e
-00200353c4012003555902200350440320035345
-042004d713052004d833062004d9e8072004da14
-082008e741092008e20c0a2008e0c90b2008e286
-00200350510120034f1902200353720320034f9b
-042004d802052004dc47062004dbc5072004d910
-082008e45a092008e3b40a2008e75b0b2008e455
-002003524f0120035517022003541e03200351fb
-'''
+generator = CM3.generator()
+while [True]:
+    print()
+    channel, value = next(generator)
+    print(channel)
+    print(value)
