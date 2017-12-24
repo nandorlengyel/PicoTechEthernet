3c7ce777bb5b91ed122defc3162dc838dfde987b
David Lutton <david@dalun.space>
2017-12-24 18:20:45 +0000

    Refactor structures for ease of use

--- client.py
+++ client.py
@@ -1,33 +1,29 @@
+"""."""
+
 import requests
 import socket
+from PicoTechEthernet import PicoTechEthernetCM3
 
-from PicoTechEthernet import PicoTechEthernet as PicoTechEthernet
+CM3 = PicoTechEthernetCM3(ip='192.168.57.200', port=6554)
 
-socket.setdefaulttimeout(1)
-CM3 = PicoTechEthernet(model=b"CM3", ip='192.168.57.200', port=6554)
-
-while [True]:
+while True:  # Loop forever
     try:
         print(CM3.connect())
         print(CM3.lock())
         CM3.filter(50)
         # print(CM3.EEPROM())
-        CM3.set("1w", b'Converting\x00')  # needs channel setup information
+        CM3.set('1w', b'Converting\x00')  # channel setup ??
 
-        generator = CM3.generator()
-        # generator, a function that yields(values) when next() is called against it.
+        for load in next(CM3):
+            print(load)
 
-        while [True]:
-            print()
-            channel, value = next(generator)
-            print(channel)
-            print(value)
+            # Submit information to a time series database, InfluxDB
+            # eg  load = '0 value=0.21761050447821617'
+            # AKA channel 0, 0.21761050447821617 mV
+            r = requests.post('http://localhost:8086/write?db=CM3', data=load)
 
-            payload = "{0!s} value={1!s}".format(channel, value)
-            try:
-                r = requests.post("http://localhost:8086/write?db=CM3", data=payload)
-            except requests.exceptions.ConnectionError:
-                print("Failed to Connect to InfluxDB")
-
+    except requests.exceptions.ConnectionError:
+        print('Connection Error to InfluxDB')
     except socket.timeout:
-        print("Connection lost to CM3")
+        print('Connection timeout to PicoTech device')
+
